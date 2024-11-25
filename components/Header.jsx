081cf5066c9db2ad6c2b6fe396a633fed938c42e
Ganesh212001 <ganeshmahajan21062001@gmail.com>
2024-11-25 18:29:21 +0530

    AddBlog

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { assets } from '@/Assets/assets'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import Link from 'next/link'
 
 const Header = () => {
 
@@ -28,7 +29,7 @@ const Header = () => {
     <div className='py-5 px-5 md:px-12 lg:px-28'>
       <div className='flex justify-between items-center'>
         <Image src={assets.logo} width={180} alt='logo' className='w-[130px] sm:w-auto'/>
-        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow} alt=""/></button>
+        <Link href="/admin/addProduct" className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Add Blog<Image src={assets.arrow} alt=""/></Link>
       </div>
       <div className='text-center my-8'>
         <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
